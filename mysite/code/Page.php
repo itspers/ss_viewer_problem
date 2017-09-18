935aac941b7d3378dc4b634e08cbe983f648ef64
Stas  Persiyanenko <omg.its.pers@gmail.com>
2017-09-19 01:17:09 +0300

    init

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -6,63 +6,63 @@ use SilverStripe\View\ArrayData;
 
 class Page extends SiteTree
 {
-	private static $db = array();
+    private static $db = array();
 
-	private static $has_one = array();
+    private static $has_one = array();
 
 
-	public function menuList()
-	{
-		return new ArrayList([
-			new ArrayData([
-				'Title' => 'Item 1',
-				'Children' => new ArrayList([
-					new ArrayData([
-						'Title' => 'Item 1.1',
-					]),
-					new ArrayData([
-						'Title' => 'Item 1.2',
-						'Children' => new ArrayList([
-							new ArrayData([
-								'Title' => 'Item 1.2.1',
-							]),
-							new ArrayData([
-								'Title' => 'Item 1.2.2',
-								'Children' => new ArrayList([
-									new ArrayData([
-										'Title' => 'Item 1.2.2.1',
-									]),
-									new ArrayData([
-										'Title' => 'Item 1.2.2.2',
-									]),
-								])
-							]),
-						])
-					]),
-				])
-			]),
-			new ArrayData([
-				'Title' => 'Item 2',
-				'Children' => new ArrayList([
-					new ArrayData([
-						'Title' => 'Item 2.1',
-					]),
-					new ArrayData([
-						'Title' => 'Item 2.2',
-						new ArrayData([
-							'Title' => 'Item 2.2.1',
-							'Children' => new ArrayList([
-								new ArrayData([
-									'Title' => 'Item 2.2.1.1',
-								]),
-								new ArrayData([
-									'Title' => 'Item 2.2.1.2',
-								]),
-							])
-						]),
-					]),
-				])
-			]),
-		]);
-	}
+    public function menuList()
+    {
+        return new ArrayList([
+            new ArrayData([
+                'Title' => 'Item 1',
+                'Children' => new ArrayList([
+                    new ArrayData([
+                        'Title' => 'Item 1.1',
+                    ]),
+                    new ArrayData([
+                        'Title' => 'Item 1.2',
+                        'Children' => new ArrayList([
+                            new ArrayData([
+                                'Title' => 'Item 1.2.1',
+                            ]),
+                            new ArrayData([
+                                'Title' => 'Item 1.2.2',
+                                'Children' => new ArrayList([
+                                    new ArrayData([
+                                        'Title' => 'Item 1.2.2.1',
+                                    ]),
+                                    new ArrayData([
+                                        'Title' => 'Item 1.2.2.2',
+                                    ]),
+                                ])
+                            ]),
+                        ])
+                    ]),
+                ])
+            ]),
+            new ArrayData([
+                'Title' => 'Item 2',
+                'Children' => new ArrayList([
+                    new ArrayData([
+                        'Title' => 'Item 2.1',
+                    ]),
+                    new ArrayData([
+                        'Title' => 'Item 2.2',
+                        new ArrayData([
+                            'Title' => 'Item 2.2.1',
+                            'Children' => new ArrayList([
+                                new ArrayData([
+                                    'Title' => 'Item 2.2.1.1',
+                                ]),
+                                new ArrayData([
+                                    'Title' => 'Item 2.2.1.2',
+                                ]),
+                            ])
+                        ]),
+                    ]),
+                ])
+            ]),
+        ]);
+    }
 }
